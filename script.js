37434f4d63b45589172e02797529a2f8698c2edb
Christian <christiandarenzo60@gmail.com>
2022-10-29 00:10:06 +0200

    integrated the code to display the book's infos, added the icons and styled the infos, but i think i'll change it

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const authorForm = document.getElementById("authorForm")
 const pagesForm = document.getElementById("pagesForm") 
 const genreForm = document.getElementById("genreForm")
 
-const gridContainer = document.getElementById("grid-container");
-
-
 let myLibrary = [];
 
 function Book(title, author, pages, genre, status) {
@@ -23,7 +20,6 @@ function Book(title, author, pages, genre, status) {
 function addBookToLibrary() {
     const newBook = new Book(titleForm.value, authorForm.value, pagesForm.value, genreForm, "reading");
     myLibrary.push(newBook);
-    console.log(myLibrary)
 }
 
 addBtn.addEventListener("click", () => {
@@ -38,11 +34,59 @@ submitBtn.addEventListener("click", (event) => {
     } 
 })
 
+const gridContainer = document.getElementById("grid-container");
+
+const titleIcon = document.createElement("img");
+titleIcon.src = "imgs/title-icon.png";
+const authorIcon = document.createElement("img");
+authorIcon.src = "imgs/author-icon.png";
+const pagesIcon = document.createElement("img");
+pagesIcon.src = "imgs/pages-icon.png";
+const genreIcon = document.createElement("img");
+genreIcon.src = "imgs/genre-icon.png";
+const readingIcon = document.createElement("img");
+readingIcon.src = "imgs/reading-icon.png";
+
 function displayBook() {
     const bookDiv = document.createElement("div");
-    bookDiv.classList.add("bookDiv")
+    bookDiv.classList.add("bookDiv");
     gridContainer.appendChild(bookDiv);
     addBookToLibrary();
+
+    //title
+    bookDiv.appendChild(titleIcon)
+    const title = document.createElement("p");
+    title.classList.add("infos")
+    title.textContent = titleForm.value;
+    bookDiv.appendChild(title)
+
+    //author
+    bookDiv.appendChild(authorIcon)
+    const author = document.createElement("p");
+    author.classList.add("infos")
+    author.textContent = authorForm.value;
+    bookDiv.appendChild(author)
+
+    //pages
+    bookDiv.appendChild(pagesIcon)
+    const pages = document.createElement("p");
+    pages.classList.add("infos")
+    pages.textContent = pagesForm.value;
+    bookDiv.appendChild(pages)
+
+    //genre
+    bookDiv.appendChild(genreIcon)
+    const genre = document.createElement("p");
+    genre.classList.add("infos")
+    genre.textContent = genreForm.value;
+    bookDiv.appendChild(genre)
+
+    //status
+    bookDiv.appendChild(readingIcon)
+    const status = document.createElement("p");
+    status.classList.add("infos")
+    status.textContent = statusForm.value;
+    bookDiv.appendChild(status)
 }
 
   
