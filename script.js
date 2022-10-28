5ac14fb981ef794b12ac8f44a59f1d3c29b45010
Christian <christiandarenzo60@gmail.com>
2022-10-28 18:35:47 +0200

    added the select input on the form and created the function to display the books

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const addBtn = document.getElementById("add");
+
 const modal = document.getElementById("modal-container");
+const submitBtn = document.getElementById("submitBtn")
+const titleForm = document.getElementById("titleForm")
+const authorForm = document.getElementById("authorForm")
+const pagesForm = document.getElementById("pagesForm") 
+const genreForm = document.getElementById("genreForm")
+
+const gridContainer = document.getElementById("grid-container");
+
+
 let myLibrary = [];
 
-function Book(title, author, genre, pages, status) {
+function Book(title, author, pages, genre, status) {
     this.title = title;
     this.author = author;
     this.genre = genre;
@@ -11,15 +21,28 @@ function Book(title, author, genre, pages, status) {
 }
 
 function addBookToLibrary() {
-    const newBook = new Book("Ricordi", "Marco Aurelio", "Filosofia", 200, "reading");
+    const newBook = new Book(titleForm.value, authorForm.value, pagesForm.value, genreForm, "reading");
     myLibrary.push(newBook);
+    console.log(myLibrary)
 }
 
 addBtn.addEventListener("click", () => {
-    displayModal();
+    modal.style.display = "flex";
 });
 
-function displayModal() {
-    modal.style.display = "flex";
+submitBtn.addEventListener("click", (event) => {
+    if(titleForm.checkValidity() && authorForm.checkValidity() && pagesForm.checkValidity()) {
+        event.preventDefault();
+        modal.style.display = "none";
+        displayBook();
+    } 
+})
+
+function displayBook() {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("bookDiv")
+    gridContainer.appendChild(bookDiv);
+    addBookToLibrary();
 }
 
+  
